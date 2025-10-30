cfe8dbd476edd9dd12a15488dcc5bf84c4bb22c2
Horlanlacerda <horlansilva56@gmail.com>
2025-10-30 17:44:06 -0300

    feat(auth-forms): Cria a classe RegisterForm com validações iniciais

---
+++ loja/admin/formulario.py
@@ -0,0 +1,8 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+class RegistrationForm(Form):
+    nome = StringField('Nome', [validators.Length(min=4, max=25)])
+    usuario = StringField('Usuário', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=35), validators.Email()])
+    senha = PasswordField('Digite sua senha', [validators.DataRequired(),validators.EqualTo('Confirmar sua senha', message='Sua senha e confirmação não são iguais')])
+    confirmacao = PasswordField('Repita sua senha')
