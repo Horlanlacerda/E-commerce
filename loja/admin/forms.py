31a7d4dc1c091ec1f7e078d3ee0ef3729a0b1440
Horlanlacerda <horlansilva56@gmail.com>
2025-11-02 13:09:45 -0300

    feat(auth): Implementa a funcionalidade de Login
    Adiciona o template login.html e o LoginForm (WTForms). Cria o endpoint /login para exibir e processar a autenticação de usuários.

--- loja/admin/forms.py
+++ loja/admin/forms.py
@@ -4,5 +4,9 @@ class RegistrationForm(Form):
     name = StringField('Nome: ', [validators.Length(min=4, max=25)])
     username = StringField('Usuário: ', [validators.Length(min=4, max=25)])
     email = StringField('Email: ', [validators.Length(min=6, max=35)])
-    password = PasswordField('Digite sua senha: ', [validators.DataRequired(),validators.EqualTo('Confirmar sua senha: ', message='Sua senha e confirmação não são iguais')])
+    password = PasswordField('Digite sua senha: ', [validators.DataRequired(),validators.EqualTo('confirm', message='Sua senha e confirmação não são iguais')])
     confirm = PasswordField('Repita sua senha: ')
+
+class LoginForm(Form):
+    email = StringField('Email: ', [validators.Length(min=6, max=35)])
+    password = PasswordField('Digite sua senha: ', [validators.DataRequired()])
--- loja/admin/rotas.py
+++ loja/admin/rotas.py
@@ -1,13 +1,14 @@
 from flask import render_template, session, request, url_for, flash,redirect
 from loja import app, db, bcrypt
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from.models import User
 import os
 
 @app.route('/')
-
 def home():
     return render_template('admin/index.html', title='Página Admininistrativa')
+
+
 
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
@@ -16,6 +17,14 @@ def registrar():
         hash_password = bcrypt.generate_password_hash(form.password.data)
         user = User(name=form.name.data,username=form.username.data, email=form.email.data, password=hash_password)
         db.session.add(user)
-        flash(f'Obrigado {form.name.data} por registrar', type='success')
-        return redirect(url_for('login'))
+        db.session.commit() # sobe os dados para o bd
+        flash(f'Obrigado {form.name.data} por registrar', category='success')
+        return redirect(url_for('home'))
     return render_template('admin/registrar.html', form=form, title="Página de Registros")
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=LoginForm(request.form)
+    return render_template('admin/login.html', form=form, title='Página de Lógin')
