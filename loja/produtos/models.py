2e69ef3e5d10f0760bfacebde4ca7a2d5f5c4539
Horlanlacerda <horlansilva56@gmail.com>
2025-11-12 13:04:02 -0300

    feat(products-db): Cria o modelo Addproduto para cadastro de produtos
    Define a classe Addproduto com as colunas (nome, preço, estoque, descrição, etc.) e configura as chaves estrangeiras para Marca e Categoria.

--- loja/produtos/models.py
+++ loja/produtos/models.py
@@ -1,4 +1,28 @@
 from loja import db, app
+from datetime import datetime
+
+class Addproduto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Numeric(10,2), nullable=False)
+    discount = db.Column(db.Integer, default=0)
+    stock = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    colors = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)
+    marca = db.relationship('Marca', backref=db.backref('marcas'), lazy=True)
+
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+    categoria = db.relationship('Categoria', backref=db.backref('categorias'), lazy=True)
+
+    image_1 = db.Column(db.String(150), nullable=False, default='imagem.jpg')
+    image_2 = db.Column(db.String(150), nullable=False, default='imagem.jpg')
+    image_3 = db.Column(db.String(150), nullable=False, default='imagem.jpg')
+
+    def __rep__(self):
+        return '<Addproduto %r>' %self.name
 
 class Marca(db.Model):
     id = db.Column(db.Integer, primary_key=True)
