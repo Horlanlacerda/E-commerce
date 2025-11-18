58a14e4814872d3f02625aab97a4ac4fc279f5b1
Horlanlacerda <horlansilva56@gmail.com>
2025-11-18 11:37:39 -0300

    feat(ui-forms): Adiciona botões de Cancelar e finaliza o Dropdown da navbar
    Inclui links/botões 'Cancelar' nos formulários de cadastro de Produtos, Marcas e Categorias. Finaliza a estrutura HTML do Dropdown no menu principal (navbar).

--- loja/admin/rotas.py
+++ loja/admin/rotas.py
@@ -6,7 +6,7 @@ from .forms import RegistrationForm, LoginForm
 from.models import User
 import os
 
-@app.route('/')
+@app.route('/admin')
 def admin():
     if'email' not in session:
         flash('Fazer login no sistema primeiro.', category='danger')
